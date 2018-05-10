86580c4be37ea987f9b66258bb08917669fcb660
theCoderSchool <tcsbcoach@gmail.com>
2018-05-09 18:10:02 -0700

    add scrape

--- js/resume.js
+++ js/resume.js
@@ -26,3 +26,24 @@
   });
 
 })(jQuery); // End of use strict
+
+function httpGet(theUrl)
+{
+    if (window.XMLHttpRequest)
+    {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    }
+    elseh
+    {// code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function()
+    {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200)
+        {
+            return xmlhttp.responseText;
+        }
+    }
+    xmlhttp.open("GET", theUrl, false );
+    xmlhttp.send();    
+}
